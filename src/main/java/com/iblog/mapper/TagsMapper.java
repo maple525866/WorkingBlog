be46ecd1eb618200d19b881eada941e72c6473c3
maple <14118288+gao-shengkai525866@user.noreply.gitee.com>
2025-12-03 15:03:38 +0800

    feature: 添加tag功能的数据层

---
+++ src/main/java/com/iblog/mapper/TagsMapper.java
@@ -0,0 +1,22 @@
+package com.iblog.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author maple
+ * @Description
+ * @createTime:2025-12-03 14:55
+ */
+@Mapper
+public interface TagsMapper {
+    int deleteTagsByAid(Long aid);
+
+    int saveTags(@Param("tags") String[] tags);
+
+    List<Long> getTagsIdByTagName(@Param("tagNames") String[] tagNames);
+
+    int saveTags2ArticleTags(@Param("tagIds") List<Long> tagIds, @Param("aid") Long aid);
+}
